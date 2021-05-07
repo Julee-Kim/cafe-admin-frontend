00b208344caa3e756aa9c6061e6d3137b12e8914
julee kim <summerbreeze0323@gmail.com>
2021-05-07 15:24:08 +0900

    auth 관련 테스트 코드 주석 삭제

--- src/api/auth.js
+++ src/api/auth.js
@@ -11,7 +11,7 @@ function postRegister(data) {
 
 // 로그인
 function postLogin(data) {
-  return axios.post('/api/staffs/login', data, {withCredentials: true})
+  return axios.post('/api/staffs/login', data)
 }
 
 // 사용자 정보
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -2,8 +2,6 @@ import API from '../../api/auth'
 
 const state = {
   token: window.localStorage.getItem('token') ? window.localStorage.getItem('token') : false,
-  // token: localStorage.token ? localStorage.token : false,
-  // userInfo: localStorage.userInfo ? JSON.parse(localStorage.userInfo) : {}
   userInfo: window.localStorage.getItem('userInfo') ? JSON.parse(window.localStorage.getItem('userInfo')) : {}
 }
 
@@ -21,25 +19,19 @@ const mutations = {
   setToken(state, value) {
 		if (value) {
 			state.token = value
-			// localStorage.token = value
       window.localStorage.setItem('token', value)
 		} else {
 			state.token = value
-			// delete localStorage.token
       window.localStorage.removeItem('token')
 		}
   },
   // 유저 정보 저장
   setUserInfo(state, value) {
-    console.log('유저 정보 저장')
-    console.log('value =>', value)
 		if (value) {
       state.userInfo = value
-			// localStorage.userInfo = JSON.stringify(value)
       window.localStorage.setItem('userInfo', JSON.stringify(value))
     } else {
 			state.userInfo = value
-			// delete localStorage.userInfo
       window.localStorage.removeItem('userInfo')
 		}
 	}
@@ -53,10 +45,6 @@ const actions = {
     try {
       const res = await API.postLogin(info);
       if (res.data.success) {
-        // const token = window.$cookies.get('authToken')
-        // const value = document.cookie.match('(^|;) ?' + 'authToken' + '=([^;]*)(;|$)')
-        // const token = value ? value[2] : null
-        console.log('authToken => ', res.data.token)
         commit('setToken', res.data.token)
       }
 
@@ -87,10 +75,7 @@ const actions = {
   async getUserInfo({ commit }) {
     try {
       const res = await API.getUserInfo()
-      console.log('사용자 정보 success')
-      console.log(res)
       if (res.status === 200) {
-        console.log(res.data)
         commit('setUserInfo', res.data)
       }
 
