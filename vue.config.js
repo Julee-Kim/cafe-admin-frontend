0782ed3dfe252f21b7916f51a6b6466ba44359a1
julee kim <summerbreeze0323@gmail.com>
2021-05-10 14:53:10 +0900

    set index.html title

--- vue.config.js
+++ vue.config.js
@@ -18,5 +18,21 @@ module.exports = {
         changeOrigin: true
       },
     }
+  },
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Mycafe admin',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
   }
 }
